182cc9b697f1626aab5bb14346c92c3e997f67dc
Fernando TPR <it21835278@my.sliit.lk>
2024-07-03 23:26:23 +0530

    modify image slider

--- fitnessapp-9ih4lxf4whwjjn83j6uqy/components/ImageSlideShow.jsx
+++ fitnessapp-9ih4lxf4whwjjn83j6uqy/components/ImageSlideShow.jsx
@@ -1,8 +1,7 @@
-import { View, Text, Image } from 'react-native';
+import { View, Image } from 'react-native';
 import React from 'react';
 import Carousel from 'react-native-reanimated-carousel';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
-
 import { sliderImages } from '../constants/index';
 
 export default function ImageSlideShow() {
@@ -17,19 +16,20 @@ export default function ImageSlideShow() {
       autoPlayInterval={4000}
       mode="parallax"
       modeConfig={{
-        parallaxScrollingScale: 0.9,
-        parallaxScrollingOffset: 50,
+        parallaxScrollingScale: 0.90,
+        parallaxScrollingOffset: 105,
+        parallaxAdjacentItemScale:0.75
       }}
     />
   );
 }
 
-const ItemCard = ({ item, index }) => {
+const ItemCard = ({ item }) => {
   return (
-    <View style={{ width: wp(100) - 70, height: hp(25), alignSelf: 'center' }}>
+    <View className="w-[85%] h-[25vh] self-center shadow-lg bg-white rounded-3xl">
       <Image
         source={item}
-        style={{ width: '100%', height: '100%', borderRadius: 30, resizeMode: 'contain' }}
+        className="w-full h-full rounded-3xl object-contain"
       />
     </View>
   );
