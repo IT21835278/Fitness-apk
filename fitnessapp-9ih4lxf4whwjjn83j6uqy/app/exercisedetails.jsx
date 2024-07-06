e337053dba08430fb0f38a4ab5f7677a8addc4b3
Fernando TPR <it21835278@my.sliit.lk>
2024-07-06 23:53:17 +0530

    complete exersise detalis

--- fitnessapp-9ih4lxf4whwjjn83j6uqy/app/exercisedetails.jsx
+++ fitnessapp-9ih4lxf4whwjjn83j6uqy/app/exercisedetails.jsx
@@ -1,4 +1,4 @@
-import { View, Text,TouchableOpacity } from 'react-native'
+import { View, Text,TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import { Image } from 'expo-image';
@@ -12,7 +12,7 @@ export default function exerciseDetails() {
     const router=useRouter()
     console.log('item:',item);
   return (
-    <View className="flex flex-1 mt-12 bg-slate-300 rounded-t-[40px]">
+    <View className="flex flex-1 mt-16 bg-white rounded-t-[40px]">
       <View className="shadow-md bg-white rounded-t-[40px]">
         <Image
           source={{uri:item.gifUrl}}
@@ -27,9 +27,37 @@ export default function exerciseDetails() {
         className="m-3 absolute rounded-md mt-2 right-0"
       >
         <MaterialIcons name="cancel" size={hp(5)} color="red"/>
-
-
       </TouchableOpacity>
+
+      <ScrollView className="mx-4 space-y-2 mt-3 " showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom:60}}>
+        <Text style={{fontSize:hp(2)}} className="text-red-700 font-bold tracking-wide">
+          Equipment : <Text className="text-neutral-600">{item?.equipment}</Text>
+        </Text>
+
+        <Text style={{fontSize:hp(2)}} className="text-red-700 font-bold tracking-wide">
+          Target Muscles : <Text className="text-neutral-600">{item?.target}</Text>
+        </Text>
+
+        <Text style={{fontSize:hp(2)}} className="text-red-700 font-bold tracking-wide">
+          Secodary Muscles : <Text className="text-neutral-600">{item?.secondaryMuscles}</Text>
+        </Text>
+
+        <Text style={{fontSize:hp(3.5)}} className="text-red-700 font-bold tracking-wide">
+          Instruction : 
+        </Text>
+              {
+                item.instructions.split(',').map((instruction,index)=>{
+                  return(
+                    <Text key={instruction} className="text-neutral-600">
+                    {instruction}
+                  </Text>
+                  )
+
+                })
+              }
+
+      </ScrollView>
+
     </View>
   )
 }
